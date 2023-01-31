01f0cf12b27bd6a9ce03f5de34019634a2cf355f
Nathan Touchton <ntouchton@protonmail.com>
2023-01-30 18:50:42 -0600

    new file:   data_manager.py 	modified:   main.py

---
+++ data_manager.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+from requests import get, put
+
+class Sheety():
+    def __init__(self):
+        self.sheety_endpoint = get(
+            url="https://api.sheety.co/73adde8dc51c874d56a66ff4e6643923/flightDeals/prices"
+            )
+        self.sheety_endpoint.raise_for_status()
+        self.sheet_data = self.sheety_endpoint.json()["prices"]
+
+    def print_data(self):
+        pprint(self.sheet_data)
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 from pprint import pprint
 from os import environ
-from requests import get
+from requests import get, put
+from twilio.rest import Client
+from data_manager import Sheety
+
+# TODO 1: Check for flight prices.
+# TODO 2: Compare against prices on Google Sheet.
+# TODO 3: Send text if price is under the threshold.
+    # TODO 3.1: The SMS should include the departure airport IATA code, destination airport IATA code, departure city, destination city, flight price and flight dates.
+
+SHEETY_DATA = Sheety()
+
+TWILIO_SID = environ["TWILIO_SID"]
+TWILIO_KEY = environ["TWILIO_KEY"]
+
+TEQUILA_HEADER = {
+    "apikey": environ["TEQUILA_KEY"],
+}
+
+
+# TEQUILA_ENDPOINT = get("https://api.tequila.kiwi.com/v2/search", headers=TEQUILA_HEADER)
+# TEQUILA_ENDPOINT.raise_for_status()
+
+# message = client.messages.create(
+#   body="Hello from Twilio",
+#   from_="+19894742866",
+#   to="+12244062483"
+# )
+
+SHEETY_DATA.print_data()
